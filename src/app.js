552084eb6117595205a9f34e04345b078b18e032
nitinrajputji <nitinrajput12911@gmail.com>
2021-12-27 20:57:57 +0530

    change directory

---
+++ src/app.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const express = require("express");
+
+require("./db/conn");
+const app = express();
+
+const port = process.env.PORT || 8000;
+app.use(express.json());
+app.use(require("./controller/route"));
+
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("assignment/build"));
+}
+
+app.listen(port, () => {
+  console.log(`listing to the port no. ${port}`);
+});
